0721b54ea72c3ce92951f8efd4b1c983665cc083
broadcastinjune <49422910+broadcastinjune@users.noreply.github.com>
2020-10-24 11:07:11 -0500

    Initial code for Simon Game

---
+++ script.js
@@ -0,0 +1,9 @@
+var randomButtons = [];
+var 
+
+$('body').keypress(function() {
+    $('h1').text('Level 1');
+    for (i = Math.floor(Math.random() * 4) + 1; i < randomButtons.length; i++) {
+        $('button')[i].push();
+    }
+})
